ad1c8519c91fc98d45009d8351dbb983c59bc806
장대윤 <red100433@gmail.com>
2018-03-15 20:53:59 +0900

    html 수정

--- src/main/java/com/stream/controller/MainController.java
+++ src/main/java/com/stream/controller/MainController.java
@@ -4,24 +4,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.stream.service.StreamService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Controller
 @Slf4j
 public class MainController {
 	@Autowired
 	private StreamService streamService;
 	
-	@GetMapping
+	@GetMapping("streaming")
+	@ResponseBody
 	public String hello(@CookieValue("user") String ip, HttpServletRequest req, HttpServletResponse res) {
-		streamService.streaming(req, res);
+//		streamService.streaming(req, res);
 		return "hello";
+	}
+	
+	@GetMapping
+	public String index() {
+		return "home";
 	}
 }
